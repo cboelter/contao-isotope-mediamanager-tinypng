420275ac82f85c64f128d82a8a3787504ac1eedb
cboelter <christopher@boelter.eu>
2017-05-17 11:42:07 +0200

    added check for active tiny-compress-images

--- contao-module/dca/tl_settings.php
+++ contao-module/dca/tl_settings.php
@@ -1,18 +1,20 @@
 <?php
 
-/**
- * Palettes
- */
-$GLOBALS['TL_DCA']['tl_settings']['palettes']['default'] =
-    $GLOBALS['TL_DCA']['tl_settings']['palettes']['default'] . ';{tiny_compress_images_legend},tinypng_api_key';
+if (!in_array('tiny-compress-images', \ModuleLoader::getActive())) {
+    /**
+     * Palettes
+     */
+    $GLOBALS['TL_DCA']['tl_settings']['palettes']['default'] =
+        $GLOBALS['TL_DCA']['tl_settings']['palettes']['default'] . ';{tiny_compress_images_legend},tinypng_api_key';
 
-/**
- * Fields
- */
-$GLOBALS['TL_DCA']['tl_settings']['fields']['tinypng_api_key'] = array
-(
-    'label'     => &$GLOBALS['TL_LANG']['tl_settings']['tinypng_api_key'],
-    'exclude'   => true,
-    'inputType' => 'text',
-    'eval'      => array('maxlength' => 255, 'tl_class' => 'w50'),
-);
+    /**
+     * Fields
+     */
+    $GLOBALS['TL_DCA']['tl_settings']['fields']['tinypng_api_key'] = array
+    (
+        'label'     => &$GLOBALS['TL_LANG']['tl_settings']['tinypng_api_key'],
+        'exclude'   => true,
+        'inputType' => 'text',
+        'eval'      => array('maxlength' => 255, 'tl_class' => 'w50'),
+    );
+}
